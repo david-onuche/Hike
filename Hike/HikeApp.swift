346777793b18ce6fa2d4a7064603158d2a5146c9
davidonuche <iamdavidonuche@gmail.com>
2026-09-06 11:38:41 +0100

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by David Onuche on 06/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
