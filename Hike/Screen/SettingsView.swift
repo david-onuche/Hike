2e11c6afbd7e5bd109c06e2069c9eaed8a4392d4
davidonuche <iamdavidonuche@gmail.com>
2026-09-06 21:07:26 +0100

    Added Swift Sheet

---
+++ Hike/Screen/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Hike
+//
+//  Created by David Onuche on 06/09/2026.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
