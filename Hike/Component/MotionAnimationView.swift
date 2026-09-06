34915fdb8746d25998309b5fe62f3a3c669d9b7a
davidonuche <iamdavidonuche@gmail.com>
2026-09-06 20:18:47 +0100

    Done with motion Animation

---
+++ Hike/Component/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  Hike
+//
+//  Created by David Onuche on 06/09/2026.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
