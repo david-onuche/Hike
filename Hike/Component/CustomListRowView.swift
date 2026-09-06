d4a9751af21718f5c8ef92a7553172863f86f84b
davidonuche <iamdavidonuche@gmail.com>
2026-09-06 23:06:40 +0100

    List and Labeled Content

---
+++ Hike/Component/CustomListRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomListRowView.swift
+//  Hike
+//
+//  Created by David Onuche on 06/09/2026.
+//
+
+import SwiftUI
+
+struct CustomListRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomListRowView()
+}
