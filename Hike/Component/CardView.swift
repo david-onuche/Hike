193b5cb8440c04e02e1e00cb8cf65e98c40b74b5
davidonuche <iamdavidonuche@gmail.com>
2026-09-06 19:14:51 +0100

    Button Style

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -8,13 +8,32 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - PROPERTIES
+    @State private var imageNumber: Int = 1
+    @State private var randomNumber: Int = 1
+    
+    // MARK: - FUNCTIONS
+    
+    func randomImage() {
+        print("--- BUTTON WAS PRESSED ---")
+        print("--- Status: Old Image Number = \(imageNumber)")
+        repeat {
+        randomNumber = Int.random(in: 1...5)
+        print("Action: Random Number Generated = \(randomNumber)")
+        } while randomNumber == imageNumber
+        imageNumber = randomNumber
+        print("Result: New Image Number = \(imageNumber)")
+        print("--- THE END ---")
+        print("\n")
+    }
     var body: some View {
         // MARK: - CARD
         ZStack {
             CustomBackgroundView()
-            VStack(alignment: .leading) {
+            VStack{
                 // MARK: - HEADER
-                VStack {
+                VStack (
+                    alignment: .leading) {
                     HStack {
                         Text("Hiking")
                             .fontWeight(.black)
@@ -58,12 +77,34 @@ struct CardView: View {
                         )
                         .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.default,
+                            value: imageNumber)
                 }
-          //  MARK: - FOOTER
-            }
+                //  MARK: - FOOTER
+                      Button {
+                          // ACTION: Generate a random number
+                          randomImage()
+                      } label: {
+                          Text("Explore More")
+                              .font(.title2)
+                              .fontWeight(.heavy)
+                              .foregroundStyle(
+                                LinearGradient(
+                                    colors: [
+                                        .customGreenLight,
+                                        .customGreenMedium],
+                                    startPoint: .top,
+                                    endPoint: .bottom
+                                )
+                              )
+                              .shadow(color: .black.opacity(0.25), radius: 0.25, x: 1, y: 2,)
+                      }
+                      .buttonStyle(GradientButton())
+         
+          } //: ZSTACK
         } //: CARD
         .frame(width: 320, height: 570)
     }
--- Hike/Utility/GradientButtonStyle.swift
+++ Hike/Utility/GradientButtonStyle.swift
@@ -5,14 +5,30 @@
 //  Created by David Onuche on 06/09/2026.
 //
 
+import Foundation
 import SwiftUI
 
-struct GradientButtonStyle: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct GradientButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                // Conditional Statement with Nil Coalescing
+                // Condition ? A : B
+                configuration.isPressed ?
+                // A: When User pressed the Button
+                LinearGradient(colors: [.customGrayMedium, .customGrayLight], startPoint: .top, endPoint: .bottom)
+                :
+                // B: When the Button is not pressed
+                LinearGradient(
+                    colors: [
+                        .customGrayLight,
+                            .customGrayMedium],
+                    startPoint: .top,
+                    endPoint: .bottom)
+            )
+            .cornerRadius(40)
     }
-}
-
-#Preview {
-    GradientButtonStyle()
 }
